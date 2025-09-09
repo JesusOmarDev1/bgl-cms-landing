8031ae965a8d994db028a9afb2d22c974431e0f0
Jesus Omar López González <jesusomar-dev@hotmail.com>
2025-09-09 00:10:19 -0600

    UI admin panel updated v2

--- src/components/BeforeLogin/index.tsx
+++ src/components/BeforeLogin/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BeforeLogin = () => {
   return (
     <div style={{ padding: '1rem', marginBottom: '1rem', textAlign: 'center' }}>
-      <h1>Bienvenido de vuelta</h1>
+      <h1 style={{ fontSize: '2.5rem', fontWeight: 'bold' }}>Bienvenido de vuelta</h1>
       <p>Inicia sesión en tu cuenta para continuar.</p>
     </div>
   )
